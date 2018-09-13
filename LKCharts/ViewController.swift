55c6e1deb10e9ac9278a14f270f54d461df89997
HectorLiuk <lkSnail93@gmail.com>
2018-09-13 10:43:21 +0800

    Initial Commit

---
+++ LKCharts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LKCharts
+//
+//  Created by Hector on 2018/9/13.
+//  Copyright © 2018年 Hector. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
